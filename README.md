37c654309d4707c57d5e169a852b8145ce847f0f
Simon <contact@vansintjan.net>
2020-08-23 13:46:58 -0700

    Test GDToolkit, add addon, but still need some debugging.
    To do with #13

---
+++ README.md
@@ -0,0 +1,11 @@
+# Federate
+
+> A grand real time strategy learning Godot experiment.
+
+## Contributing
+
+To contribute, please make sure you have GDToolkit installed:
+
+https://github.com/Scony/godot-gdscript-toolkit
+
+TODO: hopefully there will soon be a plug-in that when enabled allow you to auto format your files. 
--- scripts/WorldManager.gd
+++ scripts/WorldManager.gd
@@ -5,7 +5,9 @@ signal new_season_start
 
 var towns = []
 
-var player_federation: Federation 
+var global_priorities = [{"name": "diligence", "dislikes": []}]
+
+var player_federation: Federation
 var months := 0
 var season := 1
 var season_length := 1
@@ -15,14 +17,16 @@ var rng := RandomNumberGenerator.new()
 var Town = preload("res://scenes/Town.tscn")
 var Federation = preload("res://scripts/Federation.gd")
 
+
 func _ready() -> void:
 	player_federation = Federation.new("Baller")
 	$SeasonsTimer.start()
 	create_town("Arkanos", Vector2(400.0, 400.0), {"federation": player_federation})
-	create_town("Babylon", Vector2(100.0, 200.0), {
-		"resources": {"food": 2}, 
-		"councils": [{ "name": "farmers", "resource": "food"}]
-	})
+	create_town(
+		"Babylon",
+		Vector2(100.0, 200.0),
+		{"resources": {"food": 2}, "councils": [{"name": "farmers", "resource": "food"}]}
+	)
 
 
 func _process(_delta) -> void:
@@ -32,7 +36,7 @@ func _process(_delta) -> void:
 
 func _on_SeasonsTimer_timeout() -> void:
 	months += 1
-	if (months % season_length == 0):
+	if months % season_length == 0:
 		season += 1
 		emit_signal("new_season_start", season)
 
@@ -41,19 +45,19 @@ func create_town(town_name: String, position: Vector2, options: Dictionary) -> T
 	var town = Town.instance()
 	town.town_name = town_name
 	town.position = position
-	if (options.has("federation")):
+	if options.has("federation"):
 		town.federations = [options.get("federation")]
 	else:
 		rng.randomize()
 		town.federations = [Federation.new("Federation %s" % rng.randi())]
-		
-	if (options.has("resources")):
+
+	if options.has("resources"):
 		town.town_resources = options.get("resources")
-		
-	if (options.has("councils")):
+
+	if options.has("councils"):
 		for council in options.get("councils"):
 			town.create_council(council.get("name"), council.get("resource"))
-	print('town councils on create ', town.town_name, ' ', town.councils)		
+	print('town councils on create ', town.town_name, ' ', town.councils)
 	add_child(town)
 	town.add_to_group("towns")
 	self.connect("new_season_start", town, "_on_world_new_season_start")
